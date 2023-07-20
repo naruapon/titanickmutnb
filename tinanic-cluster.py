987e216b65f24625a2f3eca3cc2f626e386e1dc6
naruapon <naruapon@webmail.npru.ac.th>
2023-07-20 09:26:46 +0700

    add cluster and prediction

---
+++ tinanic-cluster.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+# Load the Titanic dataset
+df = pd.read_csv('test.csv')
+
+# Data preprocessing (selecting relevant features and handling missing values)
+# For this example, we'll focus on the 'Age' and 'Fare' features
+selected_features = ['Age', 'Fare']
+df = df[selected_features].dropna()
+
+# Standardize the data (important for K-means algorithm)
+scaler = StandardScaler()
+df_scaled = scaler.fit_transform(df)
+
+# Perform K-means clustering
+num_clusters = 4  # You can choose the number of clusters based on your preference
+kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+df['cluster'] = kmeans.fit_predict(df_scaled)
+
+# Data visualization
+# Create a scatter plot for visualizing clusters
+plt.figure(figsize=(10, 6))
+sns.scatterplot(data=df, x='Age', y='Fare', hue='cluster', palette='viridis', s=80, alpha=0.8)
+plt.title('Clustering of Titanic Data')
+plt.xlabel('Age')
+plt.ylabel('Fare')
+
+# Display the plot
+plt.show()
---
+++ tinanic-prediction.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Load the Titanic dataset
+df = pd.read_csv('test.csv')
+
+# Data preprocessing (selecting relevant features and handling missing values)
+# For this example, we'll focus on the 'Pclass', 'Sex', 'Age', 'Fare', and 'Survived' features
+selected_features = ['Pclass', 'Sex', 'Age', 'Fare', 'Survived']
+df = df[selected_features].dropna()
+
+# Convert categorical variables to numeric using one-hot encoding
+df = pd.get_dummies(df, drop_first=True)
+
+# Split the data into training and testing sets
+X = df.drop('Survived', axis=1)
+y = df['Survived']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train a Logistic Regression model
+model = LogisticRegression(random_state=42)
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Calculate the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy:.2f}')
+
+# Create a confusion matrix
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+# Data visualization
+# Create a heatmap for the confusion matrix
+plt.figure(figsize=(6, 4))
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', cbar=False)
+plt.title('Confusion Matrix')
+plt.xlabel('Predicted Labels')
+plt.ylabel('True Labels')
+
+# Display the plot
+plt.show()
